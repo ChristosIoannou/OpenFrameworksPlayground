a2395c79dd6e60c7b51eab74fb8e37f16a4f9618
Christos Ioannou <christos.ioannou@outlook.com>
2022-07-31 21:34:20 +0100

    added hsb colour

--- dancingMesh/src/ofApp.h
+++ dancingMesh/src/ofApp.h
@@ -21,18 +21,22 @@ public:
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
 
+    void analyseFFT();
+
     ofSoundPlayer sound;	//Sound samplevoidkeyPressed(int key);
+    bool play;
     const int N = 256;		//Number of bands in spectrum
     std::vector<float> spectrum;      //Smoothed spectrum values
     float Rad = 500;		//Cloud raduis parameter
     float Vel = 0.1;		//Cloud points velocity parameter
     int bandRad = 1;		//Band index in spectrum, affecting Rad value
     int bandVel = 100;		//Band index in spectrum, affecting Vel value
-    const int n = 300;		//Number of cloud points
+    const int n = 500;		//Number of cloud points
     std::vector<float> tx, ty;		//Offsets for Perlin noise calculation for points
     std::vector<ofPoint> p;			//Cloud's points positions
     float time0 = 0;		//Time value, used for dt computing
     bool show_spectrum;
     float bass, mids, highs, totals;
     int red, green, blue;
+    float brightness;
 };

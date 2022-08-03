3c3e6ae1ba184afa9b340ee9fc270e683b62356e
Christos Ioannou <christos.ioannou@outlook.com>
2022-08-03 20:30:44 +0100

    remove build files and data
    song - yucatan

--- dancingMesh/src/ofApp.cpp
+++ dancingMesh/src/ofApp.cpp
@@ -3,7 +3,7 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     //Set up sound sample
-    sound.load( "05-polo_and_pan_and_channel_tres--tunnel-oma.mp3" );
+    sound.load( "112 Balam - Yucatan.mp3" );
     sound.setLoop( true );
 
     spectrum.resize(N, 0.0f);
